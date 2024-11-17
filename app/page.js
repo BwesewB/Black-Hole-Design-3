5c817525ce927659dec4ffc9e4cd7ff14680027f
Sebastian <sebastianfok@gmail.com>
2024-11-17 13:34:12 -0800

    added lemonmilk, styled title

---
+++ app/page.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <div className="bh-title">
+        <div className="bh-black">
+          <h1 className="bh-h1">B</h1>
+          <h1 className="bh-h1">L</h1>
+          <h1 className="bh-h1">A</h1>
+          <h1 className="bh-h1">C</h1>
+          <h1 className="bh-h1">K</h1>
+        </div>
+        <div className="bh-hole">
+          <h1 className="bh-h1">H</h1>
+          <h1 className="bh-h1">O</h1>
+          <h1 className="bh-h1">L</h1>
+          <h1 className="bh-h1">E</h1>
+        </div>
+
+      </div>
+      
+    </>
+  );
+}
